51d2e588a5c5f1411b4ad79b4abcd545b6ed40bc
samarara <samarara@SAMARARA-MAC>
2016-09-20 12:06:20 -0400

    Working commit. Wired up event handler

--- SignalRChat/App_Start/BundleConfig.cs
+++ SignalRChat/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace SignalRChat
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/signalR").Include(
+                "~/Scripts/jquery.signalR-2.2.1.min.js"));
         }
     }
 }
---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.All.addNewChatMessageToPage(name, message);
+        }
+    }
+}
